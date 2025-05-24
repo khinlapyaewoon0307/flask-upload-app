61a9ffb2578e2008b57cf9a3159cdb156038b8d6
LegendaryWoon <141756516+LegendaryWoon@users.noreply.github.com>
2025-05-24 05:44:28 -1200

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filepath)
+            return f"File '{file.filename}' uploaded successfully!"
+    return render_template('upload.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
